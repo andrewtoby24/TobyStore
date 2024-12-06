8fb9635a7af4c6140294dedfe0208a2b0f3ab114
andrewtoby24 <thantkaung501@gmail.com>
2024-12-06 23:01:06 +0700

    Laravel Store Project

--- resources/views/admin/items/create.blade.php
+++ resources/views/admin/items/create.blade.php
@@ -2,34 +2,52 @@
 @section('content')
 <div class="container-fluid px-4">
     <div class="mt-4">
-        <h1 class="mt-4 d-inline">Posts</h1>
-            <a href="" class="btn btn-danger float-end">Cancel</a>
+        <h1 class="mt-4 d-inline">Items</h1>
+            <a href="" class="btn btn-danger float-end">Cancel Item</a>
                 </div>
                     <ol class="breadcrumb mb-4">
                         <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                        <li class="breadcrumb-item"><a href="">Posts</a></li>
-                        <li class="breadcrumb-item active">Posts</li>
+                        <li class="breadcrumb-item"><a href="">Items</a></li>
+                        <li class="breadcrumb-item active">Items Create</li>
                     </ol>
                     <div class="card mb-4">
                         <div class="card-header">
                             <i class="fas fa-table me-1"></i>
-                                Create Posts
+                                Items List
                         </div>
                         <div class="card-body">
                             <form action="">
                                 <div class="mb-3">
-                                    <label for="title" class="form-label">Title</label>
-                                    <input type="text" class="form-control" id="title" name="title">
+                                    <label for="title" class="form-label">Code No</label>
+                                    <input type="text" class="form-control" id="code_no" name="code_no">
                                 </div>
                                 <div class="mb-3">
-                                    <label for="category_id" class="form-label">Categories</label>
-                                    <select class="form-select" name="category_id" id="category_id">
-                                        <option selected>Choose....</option>
-                                    </select>
+                                    <label for="title" class="form-label">Name</label>
+                                    <input type="text" class="form-control" id="name" name="name">
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label" for="image">Image</label>
                                     <input type="file" class="form-control" id="image" name="image" aria-label="Upload">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="title" class="form-label">Price</label>
+                                    <input type="text" class="form-control" id="price" name="price">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="title" class="form-label">Discount</label>
+                                    <input type="text" class="form-control" id="discount" name="discount">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="in_stock" class="form-label">In Stock</label>
+                                    <select class="form-select" name="in_stock" id="in_stock">
+                                        <option selected>Yes</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="category_id" class="form-label">Category</label>
+                                    <select class="form-select" name="category_id" id="category_id">
+                                        <option selected>Choose Category</option>
+                                    </select>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label" for="description">Description</label>
